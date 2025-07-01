0aab8d27afe5cf40d3339b0066d94bd2069febb9
ConniConni <nmtgktmo@gmail.com>
2025-07-01 11:54:46 +0900

    整数以外が入力された場合、ValueErrorをキャッチし、whileループの先頭に戻す

--- wordbook.py
+++ wordbook.py
@@ -5,7 +5,13 @@ while choice_mode != 3:
     print("1: 単語登録")
     print("2: クイズ")
     print("3: 終了")
-    choice_mode = int(input(">>>"))
+
+    try:
+        choice_mode = int(input(">>>"))
+
+    except ValueError:
+        print("エラー: 半角英数字で入力してください")
+        continue
 
     if choice_mode == 1:
         print("単語登録を行います")
