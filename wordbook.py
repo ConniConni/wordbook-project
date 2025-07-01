b18e5ca37d9625a0c81d42a58beb0ce79c598e3c
ConniConni <nmtgktmo@gmail.com>
2025-07-01 12:05:01 +0900

    whileループの部分をメイン関数として定義

--- wordbook.py
+++ wordbook.py
@@ -1,24 +1,30 @@
-choice_mode = 0
+def main():
+    """単語帳アプリケーションのメイン関数"""
 
-while True:
-    print("実行したい操作の番号を入力してください")
-    print("1: 単語登録")
-    print("2: クイズ")
-    print("3: 終了")
+    while True:
+        print("実行したい操作の番号を入力してください")
+        print("1: 単語登録")
+        print("2: クイズ")
+        print("3: 終了")
 
-    try:
-        choice_mode = int(input(">>>"))
+        try:
+            choice_mode = int(input(">>>"))
 
-    except ValueError:
-        print("エラー: 半角数字で入力してください")
-        continue
+        except ValueError:
+            print("エラー: 半角数字で入力してください")
+            continue
 
-    if choice_mode == 1:
-        print("単語登録を行います")
-    elif choice_mode == 2:
-        print("クイズを行います")
-    elif choice_mode == 3:
-        print("終了します")
-        break
-    else:
-        print("有効な数字を入力してください")
+        if choice_mode == 1:
+            print("単語登録を行います")
+        elif choice_mode == 2:
+            print("クイズを行います")
+        elif choice_mode == 3:
+            print("終了します")
+            break
+        else:
+            print("有効な数字を入力してください")
+
+
+# --- プログラムの実行開始点　---
+if __name__ == "__main__":
+    main()
