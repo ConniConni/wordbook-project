68e81eed401d6dbfb3e2714aaae07fa32a30f175
ConniConni <nmtgktmo@gmail.com>
2025-07-02 19:10:55 +0900

    メモリ保存した辞書の単語をCSVに保存する処理を関数化 #4

--- wordbook.py
+++ wordbook.py
@@ -34,7 +34,7 @@ def main():
             print("有効な数字を入力してください")
 
 
-def register_word(word):
+def register_word(word_dict):
     print("=== 単語登録を行います ===")
 
     while True:
@@ -54,27 +54,29 @@ def register_word(word):
         else:
             print("エラー: 日本語訳は 漢字・ひらがな・カタカナで入力してください")
 
-    word[input_eg_word] = translation_jp_word
+    word_dict[input_eg_word] = translation_jp_word
+
+    save_data_to_csv_file(word_dict)
 
     print("以下の英単語と日本語訳を保存しました")
     print(f"英単語: {input_eg_word}")
     print(f"日本語訳: {translation_jp_word}")
 
 
-def start_quiz(word):
+def start_quiz(word_dict):
     print("=== クイズを行います ===")
-    if not word:
+    if not word_dict:
         print("登録された英単語がありません。英単語を登録してください")
         return
 
     # word_dictのキーをリストで保持し、ランダムに１つ抽出
-    all_keys = list(word.keys())
+    all_keys = list(word_dict.keys())
     choice_key = random.choice(all_keys)
 
     # キーに対応する日本語訳をプロンプトに表示し、ユーザーに英単語の入力を求める
     # 入力は半角英字のみを許可する
     while True:
-        print(f"'{word[choice_key]}'を英訳して入力してください")
+        print(f"'{word_dict[choice_key]}'を英訳して入力してください")
         answer_eg_word = input(">>> ")
         if is_half_width_alpha_only(answer_eg_word):
             break
@@ -124,6 +126,23 @@ def load_csv_file():
         print("CSVファイルは空です")
 
     return word_dict
+
+
+def save_data_to_csv_file(word_dict):
+    """
+    登録された単語をCSVに保存する
+    関数が呼び出される度に、CSVファイルにヘッダーを書き込む
+    現在の辞書全体を、CSVファイルに丸ごと上書き保存する
+    """
+
+    with open(CSV_FILE, "w", encoding="utf-8") as f:
+        # writerオブジェクトを生成する（書き込み処理が可能になる）
+        writer = csv.writer(f)
+        # ヘッダーを挿入する
+        writer.writerow(["英単語", "日本語訳"])
+        # 辞書のキー・バリューを取り出し、CSVのレコードとして書き込む
+        for eg_word, jp_word in word_dict.items():
+            writer.writerow([eg_word, jp_word])
 
 
 def is_half_width_alpha_only(text):
