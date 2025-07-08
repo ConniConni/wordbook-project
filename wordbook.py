7e0e1fd3696afd36f892e5f28c5d7bb153b5a988
ConniConni <nmtgktmo@gmail.com>
2025-07-08 19:27:45 +0900

    feat: プログラム実行時にログの設定を行う #14

--- wordbook.py
+++ wordbook.py
@@ -1,5 +1,10 @@
+from wordbook import logger_config
 from wordbook import main
+
 
 # --- プログラムの実行開始点　---
 if __name__ == "__main__":
+    # アプリケーションのログ設定処理を呼び出す
+    logger_config.setup_logging()
+    # アプリケーションを実行処理を呼び出す
     main.run_app()
